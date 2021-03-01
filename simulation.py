f8825ca6b61009eb7a67d9f595b7bd568b70bd1f
Joel Tari <jtari@sparse.laas.fr>
2021-03-01 01:11:02 +0100

    py simulation: open request for ground truth and initial positions of agents

--- simulation.py
+++ simulation.py
@@ -10,6 +10,7 @@ import random as rd
 import numpy.random as nprd
 
 # TODO introduce global flags such as type of commands, AA etc..
+# TODO dynamic change of std-dev noise
 
 # ----------------------------------------------------------------------------
 #                           Globals
@@ -25,22 +26,22 @@ world = {
     ],
     "landmarks":
     [
-        {"landmark_id": "l1", "state": {"x": 6, "y": 2}},
-        {"landmark_id": "l2", "state": {"x": 35, "y": 10}},
-        {"landmark_id": "l3", "state": {"x": 35, "y": 10}},
-        {"landmark_id": "l4", "state": {"x": 35, "y": 10}},
-        {"landmark_id": "l5", "state": {"x": 35, "y": 10}},
-        {"landmark_id": "l6", "state": {"x": 35, "y": 10}},
-        {"landmark_id": "l7", "state": {"x": 35, "y": 10}},
-        {"landmark_id": "l8", "state": {"x": 35, "y": 10}},
-        {"landmark_id": "l9", "state": {"x": 35, "y": 10}},
-        {"landmark_id": "l10", "state": {"x": 35, "y": 10}},
-        {"landmark_id": "l11", "state": {"x": 35, "y": 10}},
-        {"landmark_id": "l12", "state": {"x": 35, "y": 10}},
-        {"landmark_id": "l13", "state": {"x": 35, "y": 10}},
-        {"landmark_id": "l14", "state": {"x": 36, "y": 10}},
-        {"landmark_id": "l15", "state": {"x": 35, "y": 10}},
-        {"landmark_id": "l16", "state": {"x": 36, "y": 10}},
+        {"landmark_id": "l1", "state": {"x": 9, "y": 46}},
+        {"landmark_id": "l2", "state": {"x": 13, "y": 4}},
+        {"landmark_id": "l3", "state": {"x": 13.5, "y": 55}},
+        {"landmark_id": "l4", "state": {"x": 14, "y": 30}},
+        {"landmark_id": "l5", "state": {"x": 27, "y": 31}},
+        {"landmark_id": "l6", "state": {"x": 21, "y": 34}},
+        {"landmark_id": "l7", "state": {"x": 45, "y": 10}},
+        {"landmark_id": "l8", "state": {"x": 45, "y": 50}},
+        {"landmark_id": "l9", "state": {"x": 61, "y": 11}},
+        {"landmark_id": "l10", "state": {"x": 63, "y": 46}},
+        {"landmark_id": "l11", "state": {"x": 67, "y": 27}},
+        {"landmark_id": "l12", "state": {"x": 71, "y": 51}},
+        {"landmark_id": "l13", "state": {"x": 77, "y": 16}},
+        {"landmark_id": "l14", "state": {"x": 80, "y": 23}},
+        {"landmark_id": "l15", "state": {"x": 84, "y": 35}},
+        {"landmark_id": "l16", "state": {"x": 96, "y": 56}},
     ]
 }
 # store positions ini of each agent
@@ -84,6 +85,7 @@ broker = 'localhost'
 cmd_topic = 'cmd'
 cmd_feedback_topic = 'cmd_feedback'
 measures_topic = 'measures'
+request_ground_truth_topic = 'request_ground_truth'
 ground_truth_topic = 'ground_truth'
 request_position_ini_topic = 'request_position_ini'
 position_ini_topic = 'position_ini'
@@ -181,7 +183,7 @@ def in_sensor_coverage(sensorPos: dict, target: dict, sensor_info: dict) -> bool
 def generate_covariance_noise(exact_vect: np.ndarray, std_dev_stats: list, isNoiseAxisAligned=False, maxCovarianceSkew=math.pi/8) -> np.ndarray:
     # axis aligned covariance I want for odom measurement
     cov_AA = np.square(np.diag(std_dev_stats)
-                        @ np.diag(exact_vect.reshape(2,).tolist()))
+                       @ np.diag(exact_vect.reshape(2,).tolist()))
 
     if not isNoiseAxisAligned:
         # a rotational angle is defined randomly to skew the axis aligned covariance
@@ -203,7 +205,85 @@ def integrate_cumulative_odometry(cmd_cov: np.ndarray) -> None:
     #       In that case, apply the recursive linearized filter, which the update part of the traditional EKF SLAM in 2.5d
 
 # ----------------------------------------------------------------------------
-#                           mqtt callbacks
+#                        User defined callback funtions
+# ----------------------------------------------------------------------------
+
+
+def cmd_vel_callback(client, message):
+    print('cmd')
+    received_cmd = json.loads(message.payload)
+    # 1 noisify the order and update cumulative odom cov
+    noisy_cmd, cov_cmd = nosify_cmd(received_cmd['cmd_vel'])
+    print('cov')
+    print(cov_cmd)
+    integrate_cumulative_odometry(cov_cmd)
+    # TODO publish cmd feedback not at every cmd, device some shenaningans (but lets keep it 1 thread)
+    client.publish(cmd_feedback_topic, json.dumps(
+        {'robot_id': received_cmd['robot_id'], 'type': 'AAcmd', 'cmd': noisy_cmd.reshape(2,).tolist(), 'cmd_cov': cov_cmd.reshape(4,).tolist()}))
+    # 2 update robot position
+    # retrieve a pointer to the robot in the structure
+    robot_gidx = (i for i, d in enumerate(
+        world['robots']) if d['robot_id'] == received_cmd['robot_id'])
+    # stopit error if this robot_id doesnt exist
+    robot_idx = next(robot_gidx)
+    robot = world['robots'][robot_idx]
+    # retrieve a pointer to the robot position & make a copy
+    robotPos = robot['state']
+    cur_state = copy.copy(robotPos)
+    # use np arrays for operations
+    # TODO: dont forget theta
+    npar_cur_state = np.array([[robotPos['x'], robotPos['y']]]).T
+    npar_new_state = npar_cur_state + noisy_cmd
+    print(noisy_cmd)
+    # back to dictionaries to save in the world structure
+    robotPos['x'] = npar_new_state[0, 0]
+    robotPos['y'] = npar_new_state[1, 0]
+    print(f"new state   {npar_new_state}")
+    # # some trace
+    # print('Current state of robot ' + order['id'])
+    # print(cur_state)
+    # print('New state :')
+    # print(world['robots'][idx]['state'])
+
+    # 3 publish ground truth
+    client.publish(ground_truth_topic, json.dumps(world))
+    # 4 check if new odom measurement should be generated
+    # 4.1 if no, return
+    global last_pose
+    robot_last_pose_node = last_pose[robot_idx]['state']
+    if sqrt_dist(robotPos, robot_last_pose_node) < new_pose_distance_threshold:
+        return
+
+        # 4.2 if yes, generate sensor suite
+    else:
+        # 4.2.1 generate odom measure
+        odom_mes = generate_odom_measurement(
+            robotPos, robot_last_pose_node)
+        # 4.2.2 generate robot to landmarks measure
+        #       np arrays are not json serializable, so I to convert them to list first
+        #       with the np::tolist() method
+        sensor_info = robot['sensor']
+        # comprehension list
+        gen_landmarks_measurements = (
+            measure_robot_landmark(robotPos, l)
+            for l in world['landmarks']
+            if in_sensor_coverage(robotPos, l['state'], sensor_info)
+        )
+        # cast the measures generator as a list
+        landmarks_mes = list(gen_landmarks_measurements)
+        # 4.2.3 save the new robot position as the last pose node
+        robot_last_pose_node = robotPos  # TODO check that its a proper value affectation
+        last_pose[robot_idx]['state'] = copy.deepcopy(robotPos)
+        # 4.2.4 publish the measurements in the same package
+        mes_payload = {"robot_id": robot_idx,
+                       "odom": odom_mes,
+                       "landmarks": landmarks_mes
+                       }
+        client.publish(measures_topic, json.dumps(mes_payload))
+
+
+# ----------------------------------------------------------------------------
+#         mqtt layer callbacks overrides (not yet users callbacks)
 # ----------------------------------------------------------------------------
 
 
@@ -216,9 +296,10 @@ def on_connect(client, userdata, flags, rc):
         print('connected')
     else:
         print('connection error. code :', rc)
-
+    # do the subscriptions here
     client.subscribe(cmd_topic)
     client.subscribe(request_position_ini_topic)
+    client.subscribe(request_ground_truth_topic)
 
 
 def on_disconnect(client, userdata, flags, rc=0):
@@ -229,86 +310,23 @@ def on_message(client, userdata, message):
     print("Received message '" + str(message.payload) + "' on topic '"
           + message.topic + "' with QoS " + str(message.qos) + "'\n")
 
+    # depending on the topic, disptach to the user defined functions
     if message.topic == cmd_topic:
-        print('cmd')
-        received_cmd = json.loads(message.payload)
-        # 1 noisify the order and update cumulative odom cov
-        noisy_cmd, cov_cmd = nosify_cmd(received_cmd['cmd_vel'])
-        print('cov')
-        print(cov_cmd)
-        integrate_cumulative_odometry(cov_cmd)
-        # TODO publish cmd feedback not at every cmd, device some shenaningans (but lets keep it 1 thread)
-        client.publish(cmd_feedback_topic, json.dumps(
-            {'robot_id': received_cmd['robot_id'], 'type': 'AAcmd', 'cmd': noisy_cmd.reshape(2,).tolist(), 'cmd_cov': cov_cmd.reshape(4,).tolist()}))
-        # 2 update robot position
-        # retrieve a pointer to the robot in the structure
-        robot_gidx = (i for i, d in enumerate(
-            world['robots']) if d['robot_id'] == received_cmd['robot_id'])
-        # stopit error if this robot_id doesnt exist
-        robot_idx = next(robot_gidx)
-        robot = world['robots'][robot_idx]
-        # retrieve a pointer to the robot position & make a copy
-        robotPos = robot['state']
-        cur_state = copy.copy(robotPos)
-        # use np arrays for operations
-        # TODO: dont forget theta
-        npar_cur_state = np.array([[robotPos['x'], robotPos['y']]]).T
-        npar_new_state = npar_cur_state + noisy_cmd
-        print(noisy_cmd)
-        # back to dictionaries to save in the world structure
-        robotPos['x'] = npar_new_state[0, 0]
-        robotPos['y'] = npar_new_state[1, 0]
-        print(f"new state   {npar_new_state}")
-        # # some trace
-        # print('Current state of robot ' + order['id'])
-        # print(cur_state)
-        # print('New state :')
-        # print(world['robots'][idx]['state'])
-
-        # 3 publish ground truth
+        cmd_vel_callback(client, message)
+    elif message.topic == request_ground_truth_topic:
         client.publish(ground_truth_topic, json.dumps(world))
-        # 4 check if new odom measurement should be generated
-        # 4.1 if no, return
-        global last_pose
-        robot_last_pose_node = last_pose[robot_idx]['state']
-        if sqrt_dist(robotPos, robot_last_pose_node) < new_pose_distance_threshold:
-            return
+    elif message.topic == request_position_ini_topic:
+        client.publish(position_ini_topic, json.dumps(robots_ini_poses))
 
-            # 4.2 if yes, generate sensor suite
-        else:
-            # 4.2.1 generate odom measure
-            odom_mes = generate_odom_measurement(
-                robotPos, robot_last_pose_node)
-            # 4.2.2 generate robot to landmarks measure
-            #       np arrays are not json serializable, so I to convert them to list first
-            #       with the np::tolist() method
-            sensor_info = robot['sensor']
-            # comprehension list
-            gen_landmarks_measurements = (
-                measure_robot_landmark(robotPos, l)
-                for l in world['landmarks']
-                if in_sensor_coverage(robotPos, l['state'], sensor_info)
-            )
-            # cast the measures generator as a list
-            landmarks_mes = list(gen_landmarks_measurements)
-            # 4.2.3 save the new robot position as the last pose node
-            robot_last_pose_node = robotPos  # TODO check that its a proper value affectation
-            last_pose[robot_idx]['state'] = copy.deepcopy(robotPos)
-            # 4.2.4 publish the measurements in the same package
-            mes_payload = {"robot_id": robot_idx,
-                           "odom": odom_mes,
-                           "landmarks": landmarks_mes
-                           }
-            client.publish(measures_topic, json.dumps(mes_payload))
+    # if firstTime:
+    #      # publish
+    # print('publishing initial pose for robots')
+# # TODO: make it accessible on demand
+    #  request_position_ini_topic
+    #  position_ini_topic
 
-    if firstTime:
-        # publish
-        print('publishing initial pose for robots')
-        # TODO: make it accessible on demand
-        request_position_ini_topic
-        position_ini_topic
 
-    print('\n')
+# print('\n')
 
 
 # -----------------------------------------------------------------------------
