fix null covariance provoking a browser error

simulation.py
@@ -149,7 +149,7 @@ def noisify_cmd_DD(exact_cmd_vec: np.ndarray):
     alpha1 = 0.0029 #-> 5% sigma on v
     # alpha2 = 0.0002 #-> 1.4% sigma of w on v
     # alpha3 = 0.00002#-> 0.44% sigma of v on w
-    alpha4 = 0.00003 #-> 5% sigma on v
+    alpha4 = 0.00008 #-> 5% sigma on v
     alpha2=0.00002
     alpha3=0.00002
     dt=1
@@ -218,12 +218,15 @@ def measure_robot_landmark(robotstate: dict, landmark: dict) -> dict:
 #     cumulative_odom_cov = np.zeros([2, 2])
 
 #     return full_odom_mes
+def relative_angle(sensorPos: dict, target: dict):
+    angle_world_frame = math.atan2(target['y'] - sensorPos['y']
+                                  ,target['x'] - sensorPos['x'])
+    return ecpi(angle_world_frame-sensorPos['th'])
 
 
 def in_sensor_coverage(sensorPos: dict, target: dict, sensor_info: dict) -> bool:
-    # TODO: add angle coverage
     isInRange = sqrt_dist(sensorPos, target) < sensor_info['range']
-    isAngleCovered = True
+    isAngleCovered = abs(relative_angle(sensorPos,target))/math.pi < sensor_info['angle_coverage']
     return isInRange and isAngleCovered
 
 def generate_transient_odom_covariance_AA(exact_vect: np.ndarray

slam_dataviz_manager.py
@@ -155,8 +155,11 @@ if __name__ == '__main__':
             odom['state']['y'] = x*math.sin(thref) + y*math.cos(thref)+ yref
             odom['state']['th'] = ecpi(odom['state']['th']+thref)
             # visual cov
-            odom['visual_covariance']=getVisualFromCovMatrix(np.array(odom['covariance']).reshape(3,3)[0:2,0:2])
-            odom['visual_covariance']['rot'] = ecpi(odom['visual_covariance']['rot']+thref)
+            try:
+                odom['visual_covariance']=getVisualFromCovMatrix(np.array(odom['covariance']).reshape(3,3)[0:2,0:2])
+                odom['visual_covariance']['rot'] = ecpi(odom['visual_covariance']['rot']+thref)
+            except ValueError:
+                pass
             client.publish(odom_topic_out, json.dumps(odom) )
             # reset the aggregate (TODO)
             # reset_aggr()