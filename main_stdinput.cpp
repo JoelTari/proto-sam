90b7645b2c77afba08ee0e06d90ef60521490932
Joel Tari <jtari@laas.fr>
2021-11-08 14:55:57 +0100

    classify files: core, test, factor_impl

---
+++ main_stdinput.cpp
@@ -0,0 +1,13 @@
+#include "sam-system.h"
+#include "factor_impl/anchor.h"
+#include "factor_impl/linear-translation.h"
+
+int main (int argc, char *argv[])
+{
+  // auto syst = SAM::SamSystem<factor::anchor, factor::linear-translation>();
+  
+  // receive the measurement from stdin (as a string that can be converted in a C++ container)
+
+
+  return 0;
+}
