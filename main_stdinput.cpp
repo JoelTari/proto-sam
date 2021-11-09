78245a153ac9b8c6e67ac4eafbeb1979d180a506
Joel Tari <joel.tari31@gmail.com>
2021-11-09 16:56:15 +0100

    easy computation for factor A & factor b in linear

--- main_stdinput.cpp
+++ main_stdinput.cpp
@@ -33,13 +33,17 @@ class LinearTranslationFactor
                         kLinearTranslationCategoryName>
 {
   public:
-  LinearTranslationFactor(const std::string&                         factor_id,
-                          const LinearTranslationFactor::var_keys_t& var_names,
-    const LinearTranslationFactor::measure_vector_t & measure,
-    const LinearTranslationFactor::measure_covariance_matrix_t & covariance)
+  LinearTranslationFactor(
+      const std::string&                                          factor_id,
+      const LinearTranslationFactor::var_keys_t&                  var_names,
+      const LinearTranslationFactor::measure_vector_t&            measure,
+      const LinearTranslationFactor::measure_covariance_matrix_t& covariance)
       : BaseFactor<LinearTranslationFactor,
                    linTranslMeta_t,
-                   kLinearTranslationCategoryName>(factor_id, var_names, measure, covariance)
+                   kLinearTranslationCategoryName>(factor_id,
+                                                   var_names,
+                                                   measure,
+                                                   covariance)
   {
   }
 
@@ -74,23 +78,43 @@ class AnchorFactor
     : public BaseFactor<AnchorFactor, AnchorMeta_t, kAnchorCategoryName>
 {
   public:
-  AnchorFactor(const std::string&              factor_id,
-               const AnchorFactor::var_keys_t& var_names,
-    const AnchorFactor::measure_vector_t & measure,
-    const AnchorFactor::measure_covariance_matrix_t & covariance)
+  AnchorFactor(const std::string&                               factor_id,
+               const AnchorFactor::var_keys_t&                  var_names,
+               const AnchorFactor::measure_vector_t&            measure,
+               const AnchorFactor::measure_covariance_matrix_t& covariance)
       : BaseFactor<AnchorFactor, AnchorMeta_t, kAnchorCategoryName>(factor_id,
-                                                                    var_names,measure,covariance)
+                                                                    var_names,
+                                                                    measure,
+                                                                    covariance)
+      , rho_(Eigen::LLT<AnchorFactor::measure_covariance_matrix_t>(
+                 covariance.inverse())
+                 .matrixU())
+      , A_(rho_ * H_)
+      , b_(rho_ * measure)
   {
+    // (Hx-z)^T Sigma^-1 (Hx-z) = || Ax-b || _Sigma ^2
+    // Define rho s.t. Sigma^-1 = rho^T * rho  => LLT decomposition of Sigma^-1
+    // and consider the upper triangular matrix to obtain rho (Hx-z)^T Sigma^-1
+    // (Hx-z) is (Hx-z)^T rho^T * rho (Hx-z)
+    // => A = rho H  and b = rho z
   }
 
-  std::tuple<jacobian_matrix_t, measure_vector_t> compute_A_b_impl()
+  std::tuple<AnchorFactor::jacobian_matrix_t, AnchorFactor::measure_vector_t>
+      compute_A_b_impl()
   {
-    jacobian_matrix_t A;
-    measure_vector_t  b;
-    // FIX: add normed jacobian/linear A here
-    return {A, b};
+    return {A_, b_};
   }
+
+
+  private:
+  static const AnchorFactor::jacobian_matrix_t
+      H_;   // NOTE: value declared out of line
+  const AnchorFactor::jacobian_matrix_t rho_;
+  const AnchorFactor::jacobian_matrix_t A_;
+  const AnchorFactor::measure_vector_t  b_;
 };
+
+AnchorFactor::jacobian_matrix_t const AnchorFactor::H_ {{1, 0}, {0, 1}};
 
 //------------------------------------------------------------------//
 //                          Main function                           //
@@ -103,9 +127,11 @@ int main(int argc, char* argv[])
   // receive the measurement from stdin (as a string that can be converted in a
   // C++ container)
 
-  AnchorFactor::measure_vector_t z{0,0};
-  AnchorFactor::measure_covariance_matrix_t Omega{{0.2,0},{0.2}};
-  auto firstFactor = AnchorFactor("f0", {"x0"},z,Omega);
+  AnchorFactor::measure_vector_t            z {0, 0};
+  AnchorFactor::measure_covariance_matrix_t Omega {{0.2, 0}, {0.2}};
+  auto firstFactor = AnchorFactor("f0", {"x0"}, z, Omega);
+
+  syst.register_new_factor<AnchorFactor>("f0", {"x0"}, z, Omega);
 
   syst.smooth_and_map();
 
