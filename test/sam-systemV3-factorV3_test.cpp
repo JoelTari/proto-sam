765e1f0236409c49732f51508eabf2c406feb583
Joel Tari <jtari@laas.fr>
2021-11-18 17:29:37 +0100

    test for sam-systemV3

---
+++ test/sam-systemV3-factorV3_test.cpp
@@ -0,0 +1,34 @@
+#include "linear-translation.hpp"
+#include "anchor.hpp"
+
+#include "sam-system.h"
+
+//------------------------------------------------------------------//
+//                               MAIN                               //
+//------------------------------------------------------------------//
+int main(int argc, char* argv[])
+{
+  // AnchorFactor A;
+  AnchorFactor::measure_vect_t            m={0,0};
+  AnchorFactor::measure_cov_t             cov;
+  LinearTranslationFactor::measure_vect_t m2={-1,0.1};
+  LinearTranslationFactor::measure_cov_t  cov2;
+
+  auto FA = AnchorFactor("f0", m, cov, {"x0"});
+  auto FB = LinearTranslationFactor("f1", m2, cov2, {"x0", "x1"});
+
+  std::cout << "Printing runtime infos of a factor : \n";
+  factor_print(FA);
+  factor_print(FB);
+
+  std::cout << "\nPrinting infos of a factor type (only static infos since it "
+               "is just a type) : \n\n";
+  factor_print<AnchorFactor>();
+  factor_print<LinearTranslationFactor>();
+
+
+  std::cout <<"\n Declaring a sam system\n";
+  auto samsyst = SAM::SamSystem<AnchorFactor,LinearTranslationFactor>();
+
+  return 0;
+}
