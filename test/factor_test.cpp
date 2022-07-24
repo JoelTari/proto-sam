5e0d9dba0cb46ef2f69398b3736afd861a29b96f
Joel Tari <joel.tari31@gmail.com>
2022-07-25 01:14:58 +0200

    format factor_test

--- test/factor_test.cpp
+++ test/factor_test.cpp
@@ -12,13 +12,13 @@ int main(int argc, char* argv[])
   //                     TEST Factor Constructors                     //
   //------------------------------------------------------------------//
   // AnchorFactor A;
-  AnchorFactor::criterion_t            m = {2.0, -1};
-  AnchorFactor::measure_cov_t             cov = AnchorFactor::measure_cov_t::Identity();
-  LinearTranslationFactor::criterion_t m2 = {-1.0, 1.0};
-  LinearTranslationFactor::measure_cov_t  cov2 = LinearTranslationFactor::measure_cov_t::Identity();
+  AnchorFactor::criterion_t              m    = {2.0, -1};
+  AnchorFactor::measure_cov_t            cov  = AnchorFactor::measure_cov_t::Identity();
+  LinearTranslationFactor::criterion_t   m2   = {-1.0, 1.0};
+  LinearTranslationFactor::measure_cov_t cov2 = LinearTranslationFactor::measure_cov_t::Identity();
 
   auto FA = AnchorFactor("f0", m, cov, {"x0"}, {});
-  auto FB = LinearTranslationFactor("f1", m2, cov2, {"x0", "x1"}, {}); // x0 sighted from x1
+  auto FB = LinearTranslationFactor("f1", m2, cov2, {"x0", "x1"}, {});   // x0 sighted from x1
   // TODO: FA_NL FB_NL
   // TODO: more factor types
 
@@ -26,13 +26,13 @@ int main(int argc, char* argv[])
   //           TEST query the factor norm at a given point            //
   //------------------------------------------------------------------//
   // make a tuple of X points (ptr to be precise) for the factors
-  AnchorFactor::composite_state_ptr_t proposalFA = AnchorFactor::make_composite({1,0});
-  auto proposalFB = LinearTranslationFactor::make_composite({-3,-1}, {-2,-2});
+  AnchorFactor::composite_state_ptr_t proposalFA = AnchorFactor::make_composite({1, 0});
+  auto proposalFB = LinearTranslationFactor::make_composite({-3, -1}, {-2, -2});
 
   // this tests the methods : compute_h_at_x compute_r_at_x
-  double norm_FA =FA.factor_norm_at(proposalFA); // expected sqrt( (2-1)^2 + (-1-0)^2 ) = 1.41
+  double norm_FA = FA.factor_norm_at(proposalFA);   // expected sqrt( (2-1)^2 + (-1-0)^2 ) = 1.41
   std::cout << "norm of FA at proposal {1, 0} : " << norm_FA << '\n';
-  double norm_FB = FB.factor_norm_at(proposalFB); // expected 0
+  double norm_FB = FB.factor_norm_at(proposalFB);   // expected 0
   std::cout << "norm of FB at proposal (  {-3, -1}, {-2, -2} ) : " << norm_FB << '\n';
   // TODO: test the other factors
 
@@ -63,19 +63,20 @@ int main(int argc, char* argv[])
 
   std::cout << "\nAccess the 2nd component of the measurement embedded in the factor \n -> measure "
             << AnchorFactor::kMeasureComponentsName[1] << " = "
-            << AnchorFactor::measure_meta_t::get_component<AnchorFactor::kMeasureComponentsName[0]>(m)
+            << AnchorFactor::measure_meta_t::get_component<AnchorFactor::kMeasureComponentsName[0]>(
+                   m)
             << "\n";
 
   // this also works
-  static constexpr const char ycomp[]  ("y");
-  std::cout << AnchorFactor::measure_meta_t::get_component<ycomp>(m) << '\n' ; 
+  static constexpr const char ycomp[]("y");
+  std::cout << AnchorFactor::measure_meta_t::get_component<ycomp>(m) << '\n';
 
   //------------------------------------------------------------------//
   //                     TEST history management                      //
   //------------------------------------------------------------------//
   auto factors_histories_container
       = FactorsHistoriesContainer<AnchorFactor, LinearTranslationFactor>();
-  auto                        factor_FA_history = FactorHistory<decltype(FA)>(FA.factor_id, {"x0"},FA.z);
+  auto factor_FA_history = FactorHistory<decltype(FA)>(FA.factor_id, {"x0"}, FA.z);
   FactorHistory<decltype(FA)> factor_FA_history_assignment_test = factor_FA_history;
   // auto cc_factor_FA_history = std::move(factor_FA_history); // OK
   factors_histories_container.insert_new_factor_history(FA.factor_id, FA);
