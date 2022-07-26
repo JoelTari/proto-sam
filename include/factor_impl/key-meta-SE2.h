141fbe77e323e7d21e11961e8554d8a646f0d301
Joel Tari <joel.tari31@gmail.com>
2022-07-27 01:36:00 +0200

    SE2 key meta, test file for SE2 (wip) as new target

---
+++ include/factor_impl/key-meta-SE2.h
@@ -0,0 +1,52 @@
+#ifndef KEY_META_SE2_H_
+#define KEY_META_SE2_H_
+
+#include "core/meta.h"
+#include <manif/manif.h>
+
+namespace __MetaKeyPose_SE2
+{
+  inline static constexpr const char pose_SE2[] = "pose_SE2";
+  inline static constexpr const char x[]   = "x";
+  inline static constexpr const char y[]   = "y";
+  inline static constexpr const char t[]   = "theta";
+  using KeyPoseSE2_t = manif::SE2d;
+  struct MetaKeyPose_SE2_t : KeyMeta<MetaKeyPose_SE2_t, pose_SE2, KeyPoseSE2_t, manif::SE2Tangentd ,x,y,t>
+  {
+    template<const char* COMPONENT>
+    static auto get_component_impl(const KeyPoseSE2_t & key_SE2)
+    {
+      if constexpr(std::string_view(COMPONENT) ==x)
+        return key_SE2.x();
+      else
+      {
+        if constexpr(std::string_view(COMPONENT)==y)
+        {
+          return key_SE2.y();
+        }
+        else
+        {
+          static_assert(std::string_view(COMPONENT)==t);
+          return key_SE2.angle();
+          // return key_SE2.rotation();
+        }
+      }
+    }
+
+    static double get_component_impl(const char* component, const KeyPoseSE2_t & key_SE2)
+    {
+      if (std::string_view(component) == x)
+        return key_SE2.x();
+      else if (std::string_view(component) == y)
+        return key_SE2.y();
+      else if (std::string_view(component) == t)
+        return key_SE2.angle();
+      else
+        throw std::runtime_error("component requested doesnt exist in key position meta");
+    }
+
+  };
+}   // namespace __MetaKeyPose_se2
+using MetaKeyPose_SE2_t = __MetaKeyPose_SE2::MetaKeyPose_SE2_t;
+
+#endif
---
+++ test/toy_SE2_test.cpp
@@ -0,0 +1,7 @@
+#include <factor_impl/key-meta-SE2.h>
+
+int main (int argc, char *argv[])
+{
+  std::cout << "hello world" << '\n';
+  return 0;
+}
