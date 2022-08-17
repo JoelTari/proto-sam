c64c5d433393c7c51aa3a900a2ef92e0549ee76b
Joel Tari <joel.tari31@gmail.com>
2022-08-17 19:21:35 +0200

    cmake: find BLAS libraries and links . Also a small eigen test code to check that BLAS calls happens.

---
+++ test/eigen_blas_routine_test.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <Eigen/Dense>
+
+// Various calls to Eigen
+// Before running this code, set the environment variable
+//       export MKL_VERBOSE=1
+// if you expect the MKL calls, then they should appear 
+// in the traces.
+
+int main (int argc, char *argv[])
+{
+
+  auto m1 = Eigen::Matrix<double, 10, 10>::Random();
+  auto m2 = Eigen::Matrix<double, 10, 10>::Random();
+  auto b = Eigen::Vector<double,10>::Random();
+
+  std::cout << m1.adjoint()*b 
+    << std::endl;
+  std::cout << 
+    m1.selfadjointView<Eigen::Upper>()*b
+    << std::endl;
+
+  auto v1 = m1.lu().solve(b);
+  std::cout<< v1 << std::endl;
+
+  Eigen::EigenSolver<Eigen::Matrix<double,10,10>> es(m2);
+  
+  auto qr = m1.householderQr();
+  std::cout << qr.solve(b)
+    << std::endl;
+
+  std::cout <<
+    m2.selfadjointView<Eigen::Upper>().llt().solve(b)
+    << std::endl;
+
+  // TODO:  PardisoLLT
+
+  return 0;
+}
