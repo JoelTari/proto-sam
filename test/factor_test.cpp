42345639955840d6519e4cc7a96aefdaeda04917
Joel Tari <joel.tari31@gmail.com>
2022-08-18 02:12:25 +0200

    cleanup factor test a bit. Comments to plan next steps

--- test/factor_test.cpp
+++ test/factor_test.cpp
@@ -3,13 +3,6 @@
 
 #include <gtest/gtest.h>
 
-
-TEST(HelloTest, BasicAssertions) {
-  EXPECT_STRNE("hello","world");
-  EXPECT_EQ(7*6,42);
-}
-
-
 TEST(Factors, Anchor2d){
   using TestedFactor_t = ::sam::Factor::Anchor2d;
 
@@ -28,27 +21,19 @@ TEST(Factors, Anchor2d){
   std::cout << sam::Factor::stringify_factor_blockliner<TestedFactor_t>();
   std::cout << " ---- \n";
 
-  // static constexpr char y[] = "y";
-  // static constexpr const char *x = TestedFactor_t::kMeasureComponentsName[0] ;
-  // std::cout << TestedFactor_t::MeasureMeta_t::get_component<x>(m) << '\n';
-  // std::cout << TestedFactor_t::MeasureMeta_t::get_component<y>(m) << '\n';
-  // // dynamic version
-  // std::cout << TestedFactor_t::MeasureMeta_t::get_component(x,m) << '\n';
-  // std::cout << TestedFactor_t::MeasureMeta_t::get_component(y,m) << '\n';
   F.get_array_keys_id();
 }
 
 TEST(Factors, LinearTranslation2d) {
   using TestedFactor_t = ::sam::Factor::LinearTranslation2d;
-  // note the *2 in the measure cov, when the measure cov eigenvalues increases, the factor norm at a given point will decrease
-  // as it is proportional to the composite
+
   TestedFactor_t::criterion_t   m   = {-1.0, 1.0};
   auto cov = TestedFactor_t::measure_cov_t::Identity()/2;
 
   auto F = TestedFactor_t("f1", m, cov, {"x0", "x1"}, {});   // x0 sighted from x1
   auto proposalF = TestedFactor_t::make_composite({-3, -1}, {-2, -2});
 
-  double norm_F = F.factor_norm_at(proposalF);   // expected 0, here the measure cov have no effect since H.X = z
+  double norm_F = F.factor_norm_at(proposalF);
   std::cout << " ---- \n";
   EXPECT_NEAR(norm_F, 0, 1e-4);
   std::cout << " ---- \n";
@@ -60,12 +45,18 @@ TEST(Factors, LinearTranslation2d) {
   std::cout << sam::Factor::stringify_factor_blockliner<TestedFactor_t>();
   std::cout << " ---- \n";
 
-  // static constexpr char y[] = "y";
-  // static constexpr const char *x = TestedFactor_t::kMeasureComponentsName[0] ;
-  // std::cout << TestedFactor_t::MeasureMeta_t::get_component<x>(m) << '\n';
-  // std::cout << TestedFactor_t::MeasureMeta_t::get_component<y>(m) << '\n';
-  // // dynamic version
-  // std::cout << TestedFactor_t::MeasureMeta_t::get_component(x,m) << '\n';
-  // std::cout << TestedFactor_t::MeasureMeta_t::get_component(y,m) << '\n';
   F.get_array_keys_id();
 }
+
+// TODO: do the same for all instanciated factor types
+// - pose matcher SE2
+// - motion model SE2
+// - anchor SE2
+// - cartesian landmark observation SE2 -> 2d
+//
+// workflow : run the test once, note the norms, and use as basis for futur comparaison EXPECT_NEAR
+//
+// Also see this: 
+// testing::internal::CaptureStdout();
+// std::cout << "My test";
+// std::string output = testing::internal::GetCapturedStdout();
