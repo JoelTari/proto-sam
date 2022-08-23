1e4cadfd78b1fef43aaba5a75f8583c42b687a2a
Joel Tari <joel.tari31@gmail.com>
2022-08-23 18:46:01 +0200

    clean in gtests

--- test/eigen_blas_routine_test.cpp
+++ test/eigen_blas_routine_test.cpp
@@ -2,6 +2,7 @@
 #include <Eigen/Dense>
 
 #include <gtest/gtest.h>
+#include <sstream>
 
 // Various calls to Eigen
 // Before running this code, set the environment variable
@@ -9,31 +10,34 @@
 // if you expect the MKL calls, then they should appear 
 // in the traces.
 
-TEST(EigenBLASSuite, BasicBLASTest)
-{
+TEST(EigenBLASSuite, BasicBLASTest) {
+  std::stringstream ss;
 
   auto m1 = Eigen::Matrix<double, 10, 10>::Random();
   auto m2 = Eigen::Matrix<double, 10, 10>::Random();
   auto b = Eigen::Vector<double,10>::Random();
 
-  std::cout << m1.adjoint()*b 
+  ss  << m1.adjoint()*b 
     << std::endl;
-  std::cout << 
+  ss  << 
     m1.selfadjointView<Eigen::Upper>()*b
     << std::endl;
 
   auto v1 = m1.lu().solve(b);
-  std::cout<< v1 << std::endl;
+  ss << v1 << std::endl;
 
   Eigen::EigenSolver<Eigen::Matrix<double,10,10>> es(m2);
   
   auto qr = m1.householderQr();
-  std::cout << qr.solve(b)
+  ss  << qr.solve(b)
     << std::endl;
 
-  std::cout <<
+  ss  <<
     m2.selfadjointView<Eigen::Upper>().llt().solve(b)
     << std::endl;
 
   // TODO:  PardisoLLT
+
+
+  // std::cout << ss;
 }
