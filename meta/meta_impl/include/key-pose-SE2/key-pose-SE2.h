11a1592a1e22de171471cab04792675e0f39f393
Joel Tari <joel.tari31@gmail.com>
2022-09-05 04:37:31 +0200

    meta : put back details in header rather than cpp (forward declaration was a bad idea, lost a night)

--- meta/meta_impl/include/key-pose-SE2/key-pose-SE2.h
+++ meta/meta_impl/include/key-pose-SE2/key-pose-SE2.h
@@ -6,9 +6,63 @@
 #include <manif/manif.h>
 
 
-namespace details_sam::Meta::Key::PoseSE2Impl::exports
+namespace details_sam::Meta::Key
 {
-  struct PoseSE2;
+  namespace PoseSE2Impl {
+    inline static constexpr const char pose_SE2_label[] = "pose SE2";
+    inline static constexpr const char x[]   = "x";
+    inline static constexpr const char y[]   = "y";
+    inline static constexpr const char t[]   = "theta";
+    using PoseSE2_t = typename manif::SE2d;
+    using Tangent_Space_SE2_t = typename manif::SE2Tangentd;
+
+    using namespace ::sam::Meta::Key;
+
+    namespace exports{
+      struct PoseSE2 : ::sam::Meta::Key::Base<PoseSE2, pose_SE2_label, PoseSE2_t, Tangent_Space_SE2_t ,x,y,t>
+      {
+        constexpr static std::size_t compute_kN_impl()
+        {
+            return manif::SE2Tangentd::DoF;
+        }
+        
+        template<const char* COMPONENT>
+        static auto get_component_impl(const PoseSE2_t & key_SE2)
+        {
+          if constexpr(std::string_view(COMPONENT) ==x)
+            return key_SE2.x();
+          else
+          {
+            if constexpr(std::string_view(COMPONENT)==y)
+            {
+              return key_SE2.y();
+            }
+            else
+            {
+              static_assert(std::string_view(COMPONENT)==t);
+              return key_SE2.angle();
+              // return key_SE2.rotation();
+            }
+          }
+        }
+
+        static double get_component_impl(const char* component, const PoseSE2_t & key_SE2)
+        {
+          if (std::string_view(component) == x)
+            return key_SE2.x();
+          else if (std::string_view(component) == y)
+            return key_SE2.y();
+          else if (std::string_view(component) == t)
+            return key_SE2.angle();
+          else
+            throw std::runtime_error("component requested doesnt exist in key position meta");
+        }
+
+      };
+      using PoseSE2_t = typename PoseSE2::type;
+    }
+
+  }
 }
 
 namespace sam::Meta::Key{
--- meta/meta_impl/include/measure-motion-2d/measure-motion-2d.h
+++ meta/meta_impl/include/measure-motion-2d/measure-motion-2d.h
@@ -3,13 +3,61 @@
 #include <Eigen/Dense>
 #include "meta/meta.h"
 
-// forward declaration
-namespace details_sam::Meta::Measure::Motion2ddImpl::exports
+namespace details_sam::Meta::Measure
 {
-  struct Motion2d;
-}
+  namespace Motion2dImpl{
+    inline static constexpr const char linearTranslation_str[] = "linear-translation 2d";
+    inline static constexpr const char dx[]                = "dx";
+    inline static constexpr const char dy[]                = "dy";
+
+    using Motion2d_t = Eigen::Vector<double, 2>;
+    using namespace ::sam::Meta;
+
+    namespace exports{
+
+      struct Motion2d
+          : ::sam::Meta::Measure::Base<
+                          Motion2d,
+                          Motion2d_t,
+                          linearTranslation_str,
+                          dx,
+                          dy>
+      {
+        static constexpr std::size_t compute_kM_impl(){ return Motion2d_t::RowsAtCompileTime; }
+        // method where the component name is given in static
+        template <const char* COMPONENT>
+        static double get_component_impl(const Motion2d_t& measure)
+        {
+          if constexpr (std::string_view(COMPONENT) == dx)
+            return measure(0, 0);
+          else
+          {
+            if constexpr (std::string_view(COMPONENT) == dy)
+            {
+              static_assert(std::string_view(COMPONENT) == dy);
+              return measure(1, 0);
+            }
+          }
+        }
+
+        // method where the component name is given dynamic
+        static double get_component_impl(const char*                       component,
+                                         const Motion2d_t& measure)
+        {
+          if (std::string_view(component) == dx)
+            return measure(0, 0);
+          else if (std::string_view(component) == dy)
+            return measure(1, 0);
+          else
+            throw std::runtime_error("component requested doesnt exist in linear translation meta");
+        }
+      };
+      using Motion2d_t = typename Motion2d::type;
+    } // exports
+  } // Impl
+} // details_sam
 
 namespace sam::Meta::Measure
 {
-  using namespace details_sam::Meta::Measure::Motion2ddImpl::exports;
+  using namespace details_sam::Meta::Measure::Motion2dImpl::exports;
 }
