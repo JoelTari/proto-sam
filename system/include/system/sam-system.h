5d5080799102f07bccef997809c61da15d797cef
Joel Tari <joel.tari31@gmail.com>
2022-09-11 18:13:54 +0200

    split system|solver : declare solverQR class separatly from system (static only for now, but eventually it will cache ordering, R, cov (optionnaly))

--- system/include/system/sam-system.h
+++ system/include/system/sam-system.h
@@ -22,6 +22,65 @@
 namespace sam::Inference
 {
 
+  struct SolverQR
+  {
+    static std::tuple<Eigen::MatrixXd,double> compute_covariance(const Eigen::SparseMatrix<double> & A)
+    {
+      PROFILE_FUNCTION(sam_utils::JSONLogger::Instance());
+
+      auto At = Eigen::MatrixXd(A.transpose());
+      auto H = At*A;
+      return {H.inverse(),H.nonZeros()}; // inverse done through partial LU
+    }
+
+    static std::tuple<Eigen::VectorXd,double> solve(const Eigen::SparseMatrix<double>& A, const Eigen::VectorXd& b)
+    // TODO: add a solverOpts variable: check rank or not, check success, count the nnz of R or not
+    {
+      PROFILE_FUNCTION(sam_utils::JSONLogger::Instance());
+      // solver
+      Eigen::SparseQR<Eigen::SparseMatrix<double>, Eigen::COLAMDOrdering<int>> solver;
+      // MAP
+      {
+        PROFILE_SCOPE("QR decomposition",sam_utils::JSONLogger::Instance());
+        solver.compute(A);
+      }
+      // rank check: not considered a consistency check
+      auto CheckRankTooLow = [](auto &solver,auto & A)
+        { 
+          PROFILE_SCOPE("Solver Rank Check",sam_utils::JSONLogger::Instance());
+          return solver.rank() < A.cols();
+        };
+
+      if ( CheckRankTooLow(solver,A) )
+      {
+        throw std::runtime_error("RANK DEFICIENT PROBLEM");
+      }
+      auto back_substitution = [](auto & solver, auto & b)
+        {
+          PROFILE_SCOPE("Back-Substitution",sam_utils::JSONLogger::Instance());
+          Eigen::VectorXd map = solver.solve(b);
+          return map;
+        };
+      auto map = back_substitution(solver,b);
+#if ENABLE_DEBUG_TRACE
+      {
+        std::cout << "### Syst solver : " << (solver.info() ? "FAIL" : "SUCCESS") << "\n";
+        std::cout << "### Syst solver : " << (solver.info() ? "FAIL" : "SUCCESS") << "\n";
+        // std::cout << "### Syst solver :  nnz in square root : " << solver.matrixR().nonZeros()
+        //           << " (from " << A.nonZeros() << ") in Hessian."
+        //           << "\n";
+        // if ( Eigen::MatrixXd(solver.matrixR()).rows() < 15 )
+        // {
+        //   std::cout << "### Syst solver : matrix R : \n" << Eigen::MatrixXd(solver.matrixR()) << '\n';
+        // }
+      }
+#endif
+      return {map,0}; // R nnz number set at 0 (unused)
+    }
+
+   // cache : R, covariance 
+  };
+
   struct SystemHeader
   {
     std::string agent_id;
@@ -34,6 +93,7 @@ namespace sam::Inference
     {}
   };
 
+  // template <typename SOLVER_T>
   struct OptimOptions
   {
     std::size_t max_iterations = 3;  // keep it even if syst is linear
@@ -58,6 +118,7 @@ namespace sam::Inference
     // residual (actually different from the NLog from a constant offset)
   };
 
+  // template <typename SOLVER_T>
   struct OptimStats
   {
     OptimOptions optim_options; // the options inputs (the rest is more output-ish)
@@ -199,14 +260,14 @@ namespace sam::Inference
        // maximum a posteriori, may represent a \hat X or \delta \hat X (NL)
         Eigen::VectorXd MaP; 
         // give A and b to the solver
-        std::tie(MaP,rnnz) = solveQR(A,b); 
+        std::tie(MaP,rnnz) = SolverQR::solve(A,b); 
         // TODO: split the compute() step with the analyse pattern (can be set before the loop)
         // NOTE: tie() is used because structure binding declaration pose issues with lambda capture (fixed in c++20 apparently)
 
         // optionaly compute the covariance matrix
         Eigen::MatrixXd SigmaCovariance;
         double Hnnz;
-        std::tie(SigmaCovariance,Hnnz) = compute_covariance(A);
+        std::tie(SigmaCovariance,Hnnz) = SolverQR::compute_covariance(A);
         // NOTE: tie() is used because structure binding declaration pose issues with lambda capture (fixed in c++20 apparently)
 
 #if ENABLE_DEBUG_TRACE
